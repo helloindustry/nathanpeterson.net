068172aa09d59d25846fbb076f3384b60b77a077
helloindustry <nathanpeterson@gmail.com>
2020-05-21 16:54:53 -0500

    fix

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = eleventyConfig => {
   // Copy our static assets to the output folder
   eleventyConfig.addPassthroughCopy('css');
   eleventyConfig.addPassthroughCopy('assets');
+  eleventyConfig.addPassthroughCopy('blog/images');
   eleventyConfig.setLiquidOptions({
     dynamicPartials: true,
     strict_filters: true
