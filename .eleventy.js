99f174c64bb57b93fc8028e3aace8a087f38b0fa
helloindustry <nathanpeterson@gmail.com>
2020-05-25 22:23:06 -0500

    adding moment for date formatting

--- .eleventy.js
+++ .eleventy.js
@@ -10,3 +10,20 @@ module.exports = eleventyConfig => {
     strict_filters: true
   });
 };
+
+
+
+const moment = require('moment');
+
+moment.locale('en');
+
+module.exports = function (eleventyConfig) {
+
+  eleventyConfig.addFilter('dateIso', date => {
+    return moment(date).toISOString();
+  });
+
+  eleventyConfig.addFilter('dateReadable', date => {
+    return moment(date).format('LL'); // E.g. May 31, 2019
+  });
+};
